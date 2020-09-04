ea76696231e861ff402979a8d4f5f4cbdd02754d
infiduk <201912720@office.induk.ac.kr>
2020-09-04 16:33:03 +0900

    [update] 4인줄 알았던 3 내용 올리기

---
+++ 3/src/TodoContext.js
@@ -0,0 +1,60 @@
+import React, { useReducer, createContext, useContext, useRef } from 'react';
+
+const initialTodos = [
+    {
+        id: 1,
+        text: 'React flow 스터디',
+        done: true
+    },
+    {
+        id: 2,
+        text: '투두 프로젝트 구현해보기',
+        done: true
+    }
+];
+
+function todoReducer(state, action) {
+    switch (action.type) {
+        case 'CREATE':
+            return state.concat(action.todo);
+        case 'TOGGLE':
+            return state.map(todo =>
+                todo.id === action.id ? { ...todo, done: !todo.done } : todo
+            );
+        case 'REMOVE':
+            return state.filter(todo => todo.id !== action.id);
+        default:
+            throw new Error(`Unhandled action type: ${action.type}`);
+    }
+}
+
+const TodoStateContext = createContext();
+const TodoDispatchContext = createContext();
+const TodoNextIdContext = createContext();
+
+export function TodoProvider({ children }) {
+    const [state, dispatch] = useReducer(todoReducer, initialTodos);
+    const nextId = useRef(5);
+
+    return (
+        <TodoStateContext.Provider value={state}>
+            <TodoDispatchContext.Provider value={dispatch}>
+                <TodoNextIdContext.Provider value={nextId}>
+                    {children}
+                </TodoNextIdContext.Provider>
+            </TodoDispatchContext.Provider>
+        </TodoStateContext.Provider>
+    );
+}
+
+export function useTodoState() {
+    return useContext(TodoStateContext);
+}
+
+export function useTodoDispatch() {
+    return useContext(TodoDispatchContext);
+}
+
+export function useTodoNextId() {
+    return useContext(TodoNextIdContext);
+}
